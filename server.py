c4c0ed028fba9364402371c80973a2f9495cfebc
babyboldy <majokossonou@gmail.com>
2025-09-29 10:27:02 +0000

    Lancement du server

--- server.py
+++ server.py
@@ -24,6 +24,9 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
